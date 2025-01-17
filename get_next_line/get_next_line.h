61f0f491f3fd34f7259aee801227ad7e07633f22
A H M E D | أحمد <132108542+ahmedbilla@users.noreply.github.com>
2025-01-17 13:14:20 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahbilla <ahbilla@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 17:03:40 by ahbilla           #+#    #+#             */
+/*   Updated: 2024/12/16 02:38:16 by ahbilla          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strcpy(const char *src, char *dst);
+char	*ft_strdup(char *s, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+
+#endif
